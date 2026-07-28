ab00f4c6a0fd85eebcbefdc1ce7a0d8ff369a6ce
lsthc <bestesofficial@gmail.com>
2026-07-28 11:44:15 +0900

    챕터형 대시보드 변경 되돌림

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { ChapterDeck } from '@/components/ChapterDeck';
 import { DataProvider } from '@/lib/DataProvider';
+import { PresentControls, PresentModeProvider } from '@/lib/PresentMode';
+import { SmoothScrollProvider } from '@/lib/SmoothScroll';
+import { Ch0Window } from '@/chapters/Ch0_Window';
+import { Ch1Faster } from '@/chapters/Ch1_Faster';
+import { Ch2Where } from '@/chapters/Ch2_Where';
+import { Ch3Who } from '@/chapters/Ch3_Who';
+import { Ch4MyFamily } from '@/chapters/Ch4_MyFamily';
+import { Ch5WhatNow } from '@/chapters/Ch5_WhatNow';
+import { Ch6ResearchNote } from '@/chapters/Ch6_ResearchNote';
 
+/**
+ * 단일 페이지 스크롤 내러티브. 라우팅 없음. 챕터 Ch0~Ch6.
+ *
+ * Ch0~Ch3 은 심야 남색(body 배경), Ch4 부터는 챕터가 종이색 배경을 직접 깔고 시작한다.
+ * 발표 모드(P 키 · ?present=1)는 PresentModeProvider 가 담당한다.
+ */
 export default function App() {
   return (
-    <DataProvider>
-      <ChapterDeck />
-    </DataProvider>
+    <SmoothScrollProvider>
+      <PresentModeProvider>
+        <DataProvider>
+          {/* 키보드 사용자가 챕터 목록을 지나 본문으로 바로 갈 수 있게 */}
+          <a href="#ch0" className="skip-link">
+            본문으로 건너뛰기
+          </a>
+
+          <main className="min-h-screen">
+            <Ch0Window />
+            <Ch1Faster />
+            <Ch2Where />
+            <Ch3Who />
+            <Ch4MyFamily />
+            <Ch5WhatNow />
+            <Ch6ResearchNote />
+          </main>
+
+          <PresentControls />
+        </DataProvider>
+      </PresentModeProvider>
+    </SmoothScrollProvider>
   );
 }
